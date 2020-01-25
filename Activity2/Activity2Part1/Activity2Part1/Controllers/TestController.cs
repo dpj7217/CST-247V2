a3e1a29027fd46931941444b21f5613314c6fa80
dpj7217 <40005241+dpj7217@users.noreply.github.com>
2020-01-24 21:36:31 -0800

    Added Activity 2 Part 2 Code

---
+++ Activity2/Activity2Part1/Activity2Part1/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+
+            List<Activity2Part1.Models.UserModel> users = new List<Activity2Part1.Models.UserModel>();
+
+            Activity2Part1.Models.UserModel user = new Models.UserModel("Frank Brock", "HelloKitty453@Motel5.com", "(509) 555-5555");
+            Activity2Part1.Models.UserModel user2 = new Models.UserModel("Mary Poppins", "tester@yahoo.com", "(509) 555-5555");
+            Activity2Part1.Models.UserModel user3 = new Models.UserModel("Luke Sywalker", "tester@yahoo.com", "(509) 555-5555");
+            Activity2Part1.Models.UserModel user4 = new Models.UserModel("Ben Kenobi", "tester@yahoo.com", "(509) 555-5555");
+            
+            users.Add(user);
+            users.Add(user2);
+            users.Add(user3);
+            users.Add(user4);
+
+            
+            return View("Test", users);
+
+        }
+    }
+}
---
+++ Activity2/Activity2Part1/Activity2Part1/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity2Part1.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public UserModel(string name, string emailAddress, string phoneNumber)
+        {
+            this.Name = name;
+            this.EmailAddress = emailAddress;
+            this.PhoneNumber = phoneNumber;
+        }
+    }
+}
