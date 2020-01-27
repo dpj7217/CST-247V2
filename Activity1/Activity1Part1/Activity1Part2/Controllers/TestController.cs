b7b1946663402f901728c8bea9cc51068d6333c2
dpj7217 <40005241+dpj7217@users.noreply.github.com>
2020-01-26 18:08:52 -0800

    Added Parts 2 and 3

---
+++ Activity1/Activity1Part1/Activity1Part2/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1Part2.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string Index()
+        {
+            return "<b>Hello World as a string from Index</b>";
+        }
+
+        //GET: Play
+        [HttpGet]
+        public string Play()
+        {
+            return "<b>Hello Worl as a string from Play</b>";
+        }
+
+        //GET: TestView
+        [HttpGet]
+        public ActionResult TestView()
+        {
+            return View();
+        }
+    }
+}
